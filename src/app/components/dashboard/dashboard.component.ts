3f486ca42d3051f2de953a72e7c2cdf0515ba323
Adam Callanan <a.callanan@sbcglobal.net>
2020-12-26 21:16:04 -0600

    initial commit

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from "@angular/core";
+import {NavHelperService} from "src/app/services/nav-helper.service";
+import {SettingsService} from "../../services/settings.service";
+
+@Component({
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(
+    private navHelper: NavHelperService,
+    private settings: SettingsService,
+  ) {
+    this.settings.showNav = true;
+  }
+
+  public ngOnInit() {
+  }
+
+  public goToInfo() {
+    this.navHelper.goToInfo();
+  }
+
+}
